d3def0d069a56a2a95371b3a6f7161daf0f7093b
William Pratt <willpratt78@gmail.com>
2020-04-06 10:23:33 -0700

    finished updating

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,29 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contact[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i]
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function (id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contact[i]) {
+      if (this.contact[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
